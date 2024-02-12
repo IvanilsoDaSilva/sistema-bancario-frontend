d3a39324f29366a801c51f80f5cc82a14de72fc9
Micael Gomes <contato.micaeloficial@gmail.com>
2024-02-12 16:17:10 -0300

    feat: Adding a new admin page

--- src/pages/Admin/Index.tsx
+++ src/pages/Admin/Index.tsx
@@ -1,4 +1,20 @@
+import { useEffect, useState } from "react";
+const baseURL = import.meta.env.VITE_API_BASE_URL;
+
 const Index = () => {
+
+  const [users, setUsers] = useState<any[]>([])
+
+  useEffect(() => {
+    fetch(baseURL + '/account/read-all')
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setUsers(data);
+      });
+  }, []);
+
   return (
     <>
       <ol>
@@ -6,9 +22,27 @@ const Index = () => {
           <a href="/">Voltar</a>
         </ul>
       </ol>
+
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Agência</th>
+            <th>Saldo</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.agencyCode}</td>
+              <td>{user.balance}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 };
 
 export default Index;
-
