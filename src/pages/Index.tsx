75a053c4feeaab3e3b33acf722eed1199a323977
Ivanilso da Silva Conceicão Filho <ivanilsodasilva300@gmail.com>
2024-03-11 23:54:36 -0300

    REFAC: Atualização do nome da classe dos componentes

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,23 +3,19 @@ const Index = () => {
     <div className="flex flex-col min-h-[200px] space-y-5 my-page">
       <div className="flex items-center space-x-1">
         <span className="text-primary">/</span>
-        <a href="/" className="my-link">Home </a>
+        <a href="/" className="my-link">
+          Home{" "}
+        </a>
       </div>
       <div className="flex space-x-2 h-full h-min-[200px]">
-        <a
-          href="/admin"
-          className="bg-admin my-index-select-button"
-        >
+        <a href="/admin" className="bg-admin my-home-select-button">
           <div>
-            <div className="my-index-select-button-title">admin</div>
+            <div className="my-home-select-button-title">admin</div>
           </div>
         </a>
 
-        <a
-          href="/account"
-          className="bg-client my-index-select-button"
-        >
-          <div className="my-index-select-button-title">client</div>
+        <a href="/account" className="bg-client my-home-select-button">
+          <div className="my-home-select-button-title">client</div>
         </a>
       </div>
     </div>
